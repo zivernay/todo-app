56f119446a15f37eaf7f3e7adb3bbc63e44733ef
zivernay <39434401+zivernay@users.noreply.github.com>
2023-11-07 17:32:42 +0300

    Add JSDocs to todoFactory

--- src/index.js
+++ src/index.js
@@ -12,33 +12,73 @@ function todoFactory(title,
     description,
     dueDate,
     priority) {
+    /** 
+     * Get the name of the todo
+     * @function
+     * @returns {String} title - Name of the todo
+     */
     function getTitle() {
         return title;
     };
+    /** 
+     * Get the description of the todo
+     * @function
+     * @returns {String} description - Summary of the todo
+     */
     function getDescription() {
         return description;
     };
+    /** 
+     * Get the Due date of the todo
+     * @function
+     * @returns {Date} dueDate - Date by when the todo must be done
+     */
     function getDueDate() {
         return dueDate;
     };
+    /** 
+     * Get the priority of the todo
+     * @function
+     * @returns {Number} priority - 0-10 interger representing the importance(10 highest) of the note
+     */
     function getPriority() {
         return priority;
     };
+    /** 
+     * Set the title of the todo
+     * @param {String} text - Name of the todo
+     * @returns {String} text - Returns the name just assigned
+     */
     function setTitle(text) {
         title = text;
-        return true;
+        return title;
     };
+    /**
+     * Set the description of the todo
+     * @param {String} text - Short description of the todo
+     * @returns {String} description - Returns the description that has just been set
+     */
     function setDescription(text) {
         description = text;
         return true;
     };
+    /**
+     * Set when the todo must be done
+     * @param {Date} date - Date by when the todo must be done
+     * @returns {Date} dueDate - Return the date that has just been set
+     */
     function setDueDate(date) {
         dueDate = date;
-        return true;
+        return dueDate;
     };
+    /**
+     * Set the priority of the todo
+     * @param {Number} num - 0-10 interger representing the importance(10 highest) of the note
+     * @returns {Number} num - Returns the newly set value
+     */
     function setPriority(num) {
         priority = num;
-        return true;
+        return priority;
     };
     return {
         getTitle, getDescription, getDueDate, getPriority,
@@ -95,8 +135,8 @@ function projectsFactory() {
     /** Delete all categories
      * @function
      */
-    function deleteAllGroups(){
-        for (const category of Object.keys(groups)){
+    function deleteAllGroups() {
+        for (const category of Object.keys(groups)) {
             deleteGroup(category);
         }
     }
