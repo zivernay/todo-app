bb31fd9212d12c4890e3cf996ff72e7d44c65bfd
zivernay <39434401+zivernay@users.noreply.github.com>
2023-11-07 14:36:49 +0300

    Add JSdoc comments to todoFactory

--- src/index.js
+++ src/index.js
@@ -1,49 +1,49 @@
-//--- create a todo factory function  | Information holder
-/*
-    A factory function to generate todo objects
-    @params {String} title -  Name of the todo
-    @params {String} description - Description of the  of the todo task
-    @params {Datetime} dueDate - Date when task is due
-    @params {Number} priority - A number to rank how important a task is
+//--- Create a todo factory function  | Information holder
+/**
+   * A factory function to generate todo objects
+   * @constructor
+   * @params {String} title -  Name of the todo
+   * @params {String} description - Description of the  of the todo task
+   * @params {Datetime} dueDate - Date when task is due
+   * @params {Number} priority - A number to rank how important a task is
 */
 function todoFactory(title,
-        description,
-        dueDate,
-        priority) {
-            function getTitle(){
-                return title;
-            };
-            function getDescription(){
-                return description;
-            };
-            function getDueDate(){
-                return dueDate;
-            };
-            function getPriority(){
-                return priority;
-            };
-            function setTitle(text){
-                title = text;
-                return true;
-            };
-            function setDescription(text){
-                description = text;
-                return true;
-            };
-            function setDueDate(date){
-                dueDate = date;
-                return true;
-            };
-            function setPriority(num){
-                priority = num;
-                return true;
-            };
-            return {
-                getTitle, getDescription, getDueDate, getPriority,
-                setTitle, setDescription, setDueDate, setPriority,
-            }
-        }
+    description,
+    dueDate,
+    priority) {
+    function getTitle() {
+        return title;
+    };
+    function getDescription() {
+        return description;
+    };
+    function getDueDate() {
+        return dueDate;
+    };
+    function getPriority() {
+        return priority;
+    };
+    function setTitle(text) {
+        title = text;
+        return true;
+    };
+    function setDescription(text) {
+        description = text;
+        return true;
+    };
+    function setDueDate(date) {
+        dueDate = date;
+        return true;
+    };
+    function setPriority(num) {
+        priority = num;
+        return true;
+    };
+    return {
+        getTitle, getDescription, getDueDate, getPriority,
+        setTitle, setDescription, setDueDate, setPriority,
+    }
+}
 //--- create a projects factory function | Information holder
 //--- create a todoManager factory function | Service providers
 //--- create a projectsManager factory function | Service provider
- 
