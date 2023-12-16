c574f370e5e0511a9021040f2ce2bdfbd5e0c1a0
zivernay <39434401+zivernay@users.noreply.github.com>
2023-12-16 18:13:27 +0300

    Add Home screen header

--- src/app.mjs
+++ src/app.mjs
@@ -6,9 +6,4 @@ const todoObj = todoManagerFactory(todoFactory,
     projectsObj,
     todoMapObj);
 
-/* todoObj.createTodo("Note1", "Initial note");
-todoObj.createTodo("Note2", "Lets see");
-todoObj.createTodo("Note3", "Final");
-
-console.log(todoMapObj.getSize());
-console.log("hello world"); */
+    
